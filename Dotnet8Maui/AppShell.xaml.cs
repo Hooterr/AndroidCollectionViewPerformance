21158fb7b8218b6ed619c4f61db685c6dcbd6fc5
Maksymilian Lach <maks.lach99@gmail.com>
2023-10-14 16:52:11 +0200

    Initial

---
+++ Dotnet8Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Dotnet8Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Dotnet8Maui/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Dotnet8Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+        public MainPage()
+        {
+            InitializeComponent();
+            Collection.ItemsSource = Enumerable.Range(0, 10_000);
+            _stopwatch.Start();
+        }
+
+        private long _previousMs;
+        private void Collection_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            var delta = _stopwatch.ElapsedMilliseconds - _previousMs;
+
+            ScrollText.Text = (e.VerticalDelta / delta).ToString("N5");
+
+            _previousMs = _stopwatch.ElapsedMilliseconds;
+
+        }
+    }
+
+}
